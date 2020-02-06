3075e63d2407f1721491667e62f6bee5b55194d0
Tom Coemans <tom.coemans@gmail.com>
2020-02-06 17:55:43 +0100

    added export classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = { 
-    VlProperties: require('../vl-properties.src.js').VlProperties
+    VlProperties: require('../vl-properties.src.js').VlProperties,
+    VlPropertiesColumn: require('../vl-properties.src.js').VlPropertiesColumn,
+    VlPropertiesList: require('../vl-properties.src.js').VlPropertiesList,
+    VlPropertyTerm: require('../vl-properties.src.js').VlPropertyTerm,
+    VlPropertyValue: require('../vl-properties.src.js').VlPropertyValue
 }
