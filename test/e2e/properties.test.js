7d68612887bced784c9f8823f6868f63f94f2f6e
Sander Kleykens <sander.kleykens@avnu.be>
2020-03-03 08:28:23 +0100

    Introduceer e2e test

--- test/e2e/properties.test.js
+++ test/e2e/properties.test.js
@@ -1,5 +1,6 @@
 const { assert, driver } = require('vl-ui-core').Test.Setup;
 const VlPropertiesPage = require('./pages/vl-properties.page');
+const { VlPropertiesColumn } = require('./components/vl-properties');
 
 describe('vl-properties', async () => {
     const vlPropertiesPage = new VlPropertiesPage(driver);
@@ -8,7 +9,16 @@ describe('vl-properties', async () => {
         return vlPropertiesPage.load();
     });
 
-    it('dummy test omdat anders de browser vensters niet gesloten worden', () => {
-        assert.isTrue(true);
+    it('Als gebruiker kan ik properties zien in een full-size kolom', async () => {
+        const properties = await vlPropertiesPage.getProperties();
+        const propertiesChildren = await properties.getSlotElements();
+
+        assert.lengthOf(propertiesChildren, 4);
+        const fullSizeColumn = await new VlPropertiesColumn(driver, propertiesChildren[propertiesChildren.length - 1]);
+        await assert.eventually.isTrue(fullSizeColumn.isFullSize());
+
+        const propertiesList = await fullSizeColumn.getPropertiesList();
+        const nationaliteitProperty = await propertiesList.getProperty('Nationaliteit');
+        await assert.eventually.equal(nationaliteitProperty.value.getText(), 'Belg');
     });
 });
