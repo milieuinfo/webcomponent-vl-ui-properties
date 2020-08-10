67bcb529c85b055bcadcad637fc8508055434bbf
Tom Coemans <tom.coemans@gmail.com>
2020-08-10 09:27:03 +0200

    3.11.5 update

--- test/e2e/pages/vl-properties.page.js
+++ test/e2e/pages/vl-properties.page.js
@@ -2,16 +2,16 @@ const {VlProperties} = require('../components/vl-properties');
 const {Page, Config} = require('vl-ui-core').Test;
 
 class VlPropertiesPage extends Page {
-  async _getProperties(selector) {
-    return new VlProperties(this.driver, selector);
-  }
-
   async load() {
     await super.load(`${Config.baseUrl}/demo/vl-properties.html`);
   }
 
   async getProperties() {
     return this._getProperties('#properties');
+  }
+
+  async _getProperties(selector) {
+    return new VlProperties(this.driver, selector);
   }
 }
 
