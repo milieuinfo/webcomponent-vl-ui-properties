9208961449625166ecc28b1bc67968409a082177
Tom Coemans <tom.coemans@gmail.com>
2020-08-17 10:43:12 +0200

    dist cache

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  VlProperties: require('../dist/vl-properties.src.js').VlProperties,
-  VlPropertiesColumn: require('../dist/vl-properties.src.js').VlPropertiesColumn,
-  VlPropertiesList: require('../dist/vl-properties.src.js').VlPropertiesList,
-  VlPropertyTerm: require('../dist/vl-properties.src.js').VlPropertyTerm,
-  VlPropertyValue: require('../dist/vl-properties.src.js').VlPropertyValue,
-};
