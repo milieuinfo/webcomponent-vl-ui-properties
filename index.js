2533bc3ef63368f85638bfaea19032383e2e4237
Tom Coemans <tom.coemans@gmail.com>
2020-12-23 15:34:44 +0100

    e2e page objects export

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
   Test: {
-    VlProperties: require('./test/e2e/components/vl-properties'),
+    VlProperties: require('./test/e2e/components/vl-properties').VlProperties,
+    VlPropertiesColumn: require('./test/e2e/components/vl-properties').VlPropertiesColumn,
+    VlPropertiesList: require('./test/e2e/components/vl-properties').VlPropertiesList,
+    VlPropertyTerm: require('./test/e2e/components/vl-properties').VlPropertyTerm,
+    VlPropertyValue: require('./test/e2e/components/vl-properties').VlPropertyValue,
   },
 };
