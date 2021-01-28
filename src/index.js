10c1a7f0790168be1d943b4c2df622b17005706f
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 13:47:10 +0100

    ES export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-module.exports = {
-  VlProperties: require('../dist/vl-properties.src.js').VlProperties,
-  VlPropertiesColumn: require('../dist/vl-properties.src.js').VlPropertiesColumn,
-  VlPropertiesList: require('../dist/vl-properties.src.js').VlPropertiesList,
-  VlPropertyTerm: require('../dist/vl-properties.src.js').VlPropertyTerm,
-  VlPropertyValue: require('../dist/vl-properties.src.js').VlPropertyValue,
-};
+export * from '../dist/vl-properties.src.js';
